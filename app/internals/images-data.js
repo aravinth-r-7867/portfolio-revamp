ed0146f39113228f77c01576302489864d5f3136
aravinth.r <aravinth.r@zohocorp.com>
2021-04-06 20:16:43 +0530

    [CHORE] home-page-img-grid : Add home page image grid & add sass dep

---
+++ app/internals/images-data.js
@@ -0,0 +1,25 @@
+export const allImageData = {
+    "home" : [
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/kanyakumari-wave_RqUZL4_YF.JPG"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/Rameshwaram-Pamban-bridge_4phyA8QQc.JPG"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/hawa-mahal-with-sun_CqQW2q_QX.jpg"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/kanyakumari-horse_OCjKLz8b5D.JPG"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/IMG_1043_r4f6tCX1e.JPG"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/jaisalmer-maharaja-fort-architecture_m3nQt6IIg.JPG"
+        },
+        {
+            "url" : "https://ik.imagekit.io/sqexyri6dx/manali-sun_vJoWuc3vT.JPG"
+        }
+    ]
+}
